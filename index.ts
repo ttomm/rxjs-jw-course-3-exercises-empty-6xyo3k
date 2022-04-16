2f529e4d2c9ee3972880a83c0e547511ed2ab5cf
ttomm <to_scan@autograf.pl>
2022-04-16 15:53:36 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,2 +1,23 @@
+import { Observable } from 'rxjs';
 
+const observable$ = new Observable((subscriber) => {
+  console.log('Inside observable');
+  subscriber.next('First VALUE');
+  subscriber.next('Second VALUE');
+  setTimeout(() => {
+    subscriber.next('Third VALUE'); // won't be executed because of "unsubscribe" below
+    subscriber.complete(); // won't be executed because of "unsubscribe" below
+  }, 2000);
 
+  return () => {
+    console.log('TeardownLogic');
+  };
+});
+
+console.log('Before subscription');
+const sub = observable$.subscribe({
+  next: (val) => console.log(`VAL: ${val}`),
+  complete: () => console.log('Emitting finished'),
+});
+
+sub.unsubscribe();
